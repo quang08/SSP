9534d20a9d7958ae789309ca70641041fb1b1732
quang <nguyenthequang080203@gmail.com>
2025-04-17 12:30:51 +0700

    fix lint

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,6 +9,8 @@ const buttonVariants = cva(
       variant: {
         default:
           'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+        destructive:
+          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         secondary:
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         outline:
@@ -19,6 +21,7 @@ const buttonVariants = cva(
         default: 'h-9 px-4 py-2',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
+        icon: 'h-9 w-9',
       },
     },
     defaultVariants: {
